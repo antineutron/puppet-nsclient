8a90551f795fb4d3f43eb3268e2aceac1d69cc05
Andy Newton <andy.newton@snowflakesoftware.com>
2015-12-01 14:20:16 +0000

    updated package download location as it's been moved

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,9 +11,9 @@ class nsclient::params {
   $allowed_hosts           = []
   $service_state           = 'running'
   $service_enable          = true
-  $package_source_location = 'https://www.nsclient.org/files/stable'
+  $package_source_location = 'https://github.com/mickem/nscp/releases/download'
   $package_name            = 'NSClient++ (x64)'
-  $package_source          = 'NSCP-0.4.1.101-x64.msi'
+  $package_source          = '0.4.3.143/NSCP-0.4.3.143-x64.msi'
   $download_destination    = "c:\\temp"
   $config_template         = 'nsclient/nsclient.ini.erb'
   $check_disk_enabled      = true
